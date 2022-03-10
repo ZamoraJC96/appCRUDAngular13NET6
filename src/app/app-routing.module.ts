add4fbe933e970443a7aa5c85a601da166ee5777
ZamoraJC96 <zamoraleo96@gmail.com>
2022-03-10 00:10:11 -0600

    agregado router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutoComponent } from './auto/auto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: AutoComponent},
+  {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
